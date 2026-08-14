be404b8c07156d3192e92e98e767d6825df771ff
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-08-14 13:52:58 -0400

    feat(routes/auth): implement register route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,18 +1,14 @@
 import express from "express";
-import {
-    register,
-    login,
-    refreshToken,
-    logout,
-    me,
-} from "../controllers/auth.controller";
+import * as authControllers from "../controllers/auth.controller";
+import { validate } from "../middlewares/validate.middleware";
+import { registerSchema } from "../validators/auth.validator";
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
-router.post('/refresh', refreshToken);
-router.post('logout', logout);
-router.get('/me', me);
+router.post('/register', validate(registerSchema), authControllers.register);
+router.post('/login', authControllers.login);
+router.post('/refresh', authControllers.refreshToken);
+router.post('logout', authControllers.logout);
+router.get('/me', authControllers.me);
 
 export default router;
