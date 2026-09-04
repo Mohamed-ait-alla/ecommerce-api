f7c945080ffda46d400afad397d349645e28b4fe
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-09-04 05:08:55 -0400

    feat(routes/auth): implement login route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import * as authControllers from "../controllers/auth.controller";
 import { validate } from "../middlewares/validate.middleware";
-import { registerSchema } from "../validators/auth.validator";
+import { registerSchema, loginSchema } from "../validators/auth.validator";
 
 const router = express.Router();
 
 router.post('/register', validate(registerSchema), authControllers.register);
-router.post('/login', authControllers.login);
+router.post('/login', validate(loginSchema), authControllers.login);
 router.post('/refresh', authControllers.refreshToken);
 router.post('logout', authControllers.logout);
 router.get('/me', authControllers.me);
