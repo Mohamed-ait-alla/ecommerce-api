ba9f9ae435ba775937f28758b4cd045d474dcd53
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-09-06 13:19:47 -0400

    feat(routes/products): add a route for getProductById

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import * as productController from "../controllers/products.controller";
 import { validate } from "../middlewares/validate.middleware";
-import { listProductsQuerySchema } from "../validators/product.validator";
+import { listProductsQuerySchema, productIdParamSchema } from "../validators/product.validator";
 
 const router = Router();
 
 // public routes
 router.get('/', validate(listProductsQuerySchema), productController.listProducts);
+router.get('/:id', validate(productIdParamSchema), productController.getProductById);
 
 // admin routes
 
