503ade81dc9c53934e47a7a382e523fc21155dcb
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-09-06 10:59:17 -0400

    fix(middlewares): assign parsed values to request object

--- src/middlewares/validate.middleware.ts
+++ src/middlewares/validate.middleware.ts
@@ -2,19 +2,31 @@ import type { Request, Response, NextFunction } from "express";
 import { ZodError, ZodObject } from "zod";
 import { AppError } from "../utils/AppError";
 
+const updateQuery = (req: Request, value: any) => {
+    Object.defineProperty(req, "query", {
+        ...Object.getOwnPropertyDescriptor(req, "query"),
+        writable: false,
+        value,
+    });
+};
+
 export const validate =
     (schema: ZodObject) =>
     (req: Request, _res: Response, next: NextFunction) => {
         try {
-            schema.parse({ body: req.body });
+            const parsed = schema.parse({ body: req.body, query: req.query });
+
+            if (parsed.body !== undefined) req.body = parsed.body;
+            if (parsed.query !== undefined) updateQuery(req, parsed.query);
+
             next();
         } catch (error) {
             if (error instanceof ZodError) {
                 const messages = error.issues
                     .map((issue) => issue.message)
-                    .join(', ');
+                    .join(", ");
                 throw new AppError(messages, 400);
             }
             next(error);
         }
-	};
+    };
