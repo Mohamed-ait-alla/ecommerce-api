bc0bf82b42116b5ae7ba95ee9d0f81b1e3cae3e9
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-09-06 10:56:00 -0400

    feat(services/products): implement the logic of listing products with filtering and pagination

---
+++ src/services/product.service.ts
@@ -0,0 +1,47 @@
+import { prisma } from "../config/db";
+import { AppError } from "../utils/AppError";
+import type { Prisma } from "../generated/prisma/client";
+import type { ListProductsQuery } from "../validators/product.validator";
+
+export const listProducts = async (query: ListProductsQuery) => {
+    const { page, limit, search, category, minPrice, maxPrice, sort } = query;
+
+    // building where statement
+    const where: Prisma.ProductWhereInput = {
+        isActive: true,
+        ...(search && { name: { contains: search, mode: "insensitive" } }),
+        ...(category && { category: { slug: category } }),
+        ...((minPrice !== undefined || maxPrice !== undefined) && {
+            price: {
+                ...(minPrice !== undefined && { gte: minPrice }),
+                ...(maxPrice !== undefined && { lte: maxPrice }),
+            },
+        }),
+    };
+
+    const [field, direction] = sort.startsWith("-")
+        ? [sort.slice(1), "desc"]
+        : [sort, "asc"];
+
+    const [items, total] = await Promise.all([
+        prisma.product.findMany({
+            where,
+            orderBy: { [field]: direction },
+            skip: (page - 1) * limit,
+            take: limit,
+            include: {
+                category: { select: { id: true, name: true, slug: true } },
+            },
+        }),
+        prisma.product.count({ where }),
+    ]);
+
+	if (items.length < 1) {
+		throw new AppError('No items found at the moment', 404);
+	}
+
+    return {
+        items,
+        meta: { total, page, limit, totalPages: Math.ceil(total / limit) },
+    };
+};
