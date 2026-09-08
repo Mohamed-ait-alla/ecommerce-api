992b0ce2caaa6b616bd0109affee29f92cafaf48
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-09-08 13:25:18 -0400

    feat(routes/cart): add a route for adding an item to cart controller

--- src/routes/cart.route.ts
+++ src/routes/cart.route.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { requireAuth } from "../middlewares/auth.middleware";
+import { validate } from "../middlewares/validate.middleware";
+import { addCartItemSchema } from "../validators/cart.validator";
 import * as cartController from "../controllers/cart.controller";
 
 const router = Router();
@@ -8,5 +10,6 @@ const router = Router();
 router.use(requireAuth);
 
 router.get('/', cartController.getCart);
+router.post('/items', validate(addCartItemSchema), cartController.addItem);
 
 export default router;
