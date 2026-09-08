eab70ee6785f8118b4b19ad7ca39530c1a791de6
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-09-08 04:24:36 -0400

    feat(routes/category): add a route for update category controller

--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -2,7 +2,10 @@ import { Router } from "express";
 import * as categoryController from "../controllers/category.controller";
 import { validate } from "../middlewares/validate.middleware";
 import { requireAuth, requireRole } from "../middlewares/auth.middleware";
-import { createCategorySchema } from "../validators/category.validator";
+import {
+    createCategorySchema,
+    updateCategorySchema,
+} from "../validators/category.validator";
 
 const router = Router();
 
@@ -11,11 +14,19 @@ router.get('/', categoryController.listCategories);
 
 // admin only
 router.post(
-    "/",
+    '/',
     requireAuth,
-    requireRole("ADMIN"),
+    requireRole('ADMIN'),
     validate(createCategorySchema),
     categoryController.createCategory,
+);
+
+router.put(
+    '/:id',
+    requireAuth,
+    requireRole('ADMIN'),
+    validate(updateCategorySchema),
+    categoryController.updateCategory,
 );
 
 export default router;
