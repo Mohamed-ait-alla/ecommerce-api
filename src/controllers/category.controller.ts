e66aebd913a08e31b99d87c0b6799bbb0628b590
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-09-08 03:52:51 -0400

    feat(controllers/category): implement create category action

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -3,8 +3,13 @@ import * as categoryService from "../services/category.service";
 import { sendResponse } from "../utils/apiResponse";
 
 const listCategories = async (_req: Request, res: Response) => {
-	const categories = await categoryService.listCategories();
-	sendResponse(res, 200, categories);
+    const categories = await categoryService.listCategories();
+    sendResponse(res, 200, categories);
 };
 
-export { listCategories };
+const createCategory = async (req: Request, res: Response) => {
+	const category = await categoryService.createCategory(req.body);
+	sendResponse(res, 201, category, 'Category created successfully');
+};
+
+export { listCategories, createCategory };
