21c10325a4a38eee5b5d3a1de947d12dc1783cff
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-09-08 04:23:09 -0400

    feat(validators/category): add a schema validator for updating a category

--- src/validators/category.validator.ts
+++ src/validators/category.validator.ts
@@ -7,4 +7,13 @@ export const createCategorySchema = z.object({
     }),
 });
 
+export const updateCategorySchema = z.object({
+    params: z.object({ id: z.uuid() }),
+    body: z.object({
+        name: z.string().min(1).max(100).optional(),
+        description: z.string().optional(),
+    }),
+});
+
 export type CreateCategoryInput = z.infer<typeof createCategorySchema>['body'];
+export type UpdateCategoryInput = z.infer<typeof updateCategorySchema>['body'];
