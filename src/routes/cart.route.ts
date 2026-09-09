c1de7f4dc0c68f8b1d603f1047a10d72628aadfc
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-09-09 04:22:57 -0400

    feat(routes/cart): add a route for deleting an item from a cart controller

--- src/routes/cart.route.ts
+++ src/routes/cart.route.ts
@@ -3,6 +3,7 @@ import { requireAuth } from "../middlewares/auth.middleware";
 import { validate } from "../middlewares/validate.middleware";
 import {
     addCartItemSchema,
+    cartItemParamSchema,
     updateCartItemSchema,
 } from "../validators/cart.validator";
 import * as cartController from "../controllers/cart.controller";
@@ -15,5 +16,5 @@ router.use(requireAuth);
 router.get('/', cartController.getCart);
 router.post('/items', validate(addCartItemSchema), cartController.addItem);
 router.patch('/items/:productId', validate(updateCartItemSchema), cartController.updateItem);
-
+router.delete('/items/:productId', validate(cartItemParamSchema), cartController.deleteItem);
 export default router;
