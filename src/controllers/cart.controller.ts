211c3bf277c788be74b02e646ecf2339581fef05
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-09-09 03:55:01 -0400

    feat(controllers/cart): implement update cart item action

--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -1,7 +1,7 @@
 import type { Request, Response } from "express";
 import type { AuthenticatedRequest } from "../types/auth.types";
-import * as cartService from "../services/cart.service";
 import { sendResponse } from "../utils/apiResponse";
+import * as cartService from "../services/cart.service";
 
 const getCart = async (req: AuthenticatedRequest, res: Response) => {
     const cart = await cartService.getCart(req.user!.id);
@@ -9,8 +9,17 @@ const getCart = async (req: AuthenticatedRequest, res: Response) => {
 };
 
 const addItem = async (req: AuthenticatedRequest, res: Response) => {
-	const cart = await cartService.addItemToCart(req.user!.id, req.body);
-	sendResponse(res, 201, cart, 'Item added to cart')
-}
+    const cart = await cartService.addItemToCart(req.user!.id, req.body);
+    sendResponse(res, 201, cart, 'Item added to cart');
+};
 
-export { getCart, addItem };
+const updateItem = async (req: AuthenticatedRequest, res: Response) => {
+    const cart = await cartService.updateCartItem(
+        req.user!.id,
+        req.params.productId as string,
+        req.body,
+    );
+    sendResponse(res, 200, cart, 'Cart item updated');
+};
+
+export { getCart, addItem, updateItem };
