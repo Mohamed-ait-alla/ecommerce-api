d94fcf9b91d0960f4540d278e26596ba7a3483ae
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-09-10 13:18:43 -0400

    feat(routes/address): add a route for get address controller

--- src/routes/address.route.ts
+++ src/routes/address.route.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { requireAuth } from "../middlewares/auth.middleware";
+import { validate } from "../middlewares/validate.middleware";
+import { addressIdParamSchema } from "../validators/address.validator";
 import * as addressController from "../controllers/address.controller";
 
 const router = Router();
@@ -7,5 +9,6 @@ const router = Router();
 router.use(requireAuth);
 
 router.get('/', addressController.listAddresses);
+router.get('/:id', validate(addressIdParamSchema), addressController.getAddress);
 
 export default router;
