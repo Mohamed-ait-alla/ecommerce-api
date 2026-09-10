e3a15ae949e9df4ae5672e41ae2a9f17fe8c08fb
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-09-10 13:17:31 -0400

    feat(services/address): implement the logic of getting an address by id

--- src/services/address.service.ts
+++ src/services/address.service.ts
@@ -1,8 +1,21 @@
 import { prisma } from "../config/db";
+import { AppError } from "../utils/AppError";
 
 export const listAddresses = async (userId: string) => {
     return await prisma.address.findMany({
         where: { userId },
         orderBy: [{ isDefault: 'desc' }, { createdAt: 'desc' }],
     });
+};
+
+export const getAddressById = async (userId: string, addressId: string) => {
+    const address = await prisma.address.findFirst({
+        where: { id: addressId, userId },
+    });
+
+    if (!address) {
+        throw new AppError("Address not found", 404);
+    }
+
+    return address;
 };
