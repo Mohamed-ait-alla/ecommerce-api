687f722e7e301c888043056842a29080456200d0
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-09-10 15:49:12 -0400

    feat(routes/address): add a route for add a user's address action

--- src/routes/address.route.ts
+++ src/routes/address.route.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { requireAuth } from "../middlewares/auth.middleware";
 import { validate } from "../middlewares/validate.middleware";
-import { addressIdParamSchema } from "../validators/address.validator";
+import {
+    addressIdParamSchema,
+    createAddressSchema,
+} from "../validators/address.validator";
 import * as addressController from "../controllers/address.controller";
 
 const router = Router();
@@ -10,5 +13,6 @@ router.use(requireAuth);
 
 router.get('/', addressController.listAddresses);
 router.get('/:id', validate(addressIdParamSchema), addressController.getAddress);
+router.post('/', validate(createAddressSchema), addressController.createAddress);
 
 export default router;
