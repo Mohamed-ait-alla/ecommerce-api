88976191bbab464bd812961978b7599c39088b57
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-09-11 04:15:58 -0400

    feat(routes/address): add a route for update a user's address action

--- src/routes/address.route.ts
+++ src/routes/address.route.ts
@@ -4,6 +4,7 @@ import { validate } from "../middlewares/validate.middleware";
 import {
     addressIdParamSchema,
     createAddressSchema,
+	updateAddressSchema,
 } from "../validators/address.validator";
 import * as addressController from "../controllers/address.controller";
 
@@ -14,5 +15,5 @@ router.use(requireAuth);
 router.get('/', addressController.listAddresses);
 router.get('/:id', validate(addressIdParamSchema), addressController.getAddress);
 router.post('/', validate(createAddressSchema), addressController.createAddress);
-
+router.put('/:id', validate(updateAddressSchema), addressController.updateAddress);
 export default router;
