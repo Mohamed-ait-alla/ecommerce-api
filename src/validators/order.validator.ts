14aa81551979f3921fd8fa84e9044e9cb1a4fdfb
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-09-12 15:28:02 -0400

    feat(validators/order): add a schema validator for listing user orders query

--- src/validators/order.validator.ts
+++ src/validators/order.validator.ts
@@ -1,5 +1,16 @@
 import { z } from "zod";
+import { OrderStatus } from "../generated/prisma/enums";
 
 export const checkoutSchema = z.object({
     body: z.object({ addressId: z.uuid("Invalid address id") }),
 });
+
+export const listOrdersQuerySchema = z.object({
+  query: z.object({
+    page: z.coerce.number().int().positive().default(1),
+    limit: z.coerce.number().int().positive().max(15).default(5),
+    status: z.enum(OrderStatus).optional(),
+  }),
+});
+
+export type ListOrdersQuery = z.infer<typeof listOrdersQuerySchema>['query'];
