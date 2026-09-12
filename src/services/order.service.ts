394029df61d6ecf708cda085fd220676cabbad11
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-09-12 04:54:10 -0400

    feat(services/order): implement the logic of creating an order

---
+++ src/services/order.service.ts
@@ -0,0 +1,106 @@
+import { prisma } from "../config/db";
+import { Prisma } from "../generated/prisma/client";
+import { AppError } from "../utils/AppError";
+import { env } from "../validators/env.validator";
+import { stripe } from "../config/stripe";
+
+export const checkout = async (userId: string, addressId: string) => {
+    // check for user's address
+    const address = await prisma.address.findFirst({
+        where: { id: addressId, userId },
+    });
+    if (!address) {
+        throw new AppError("Address not found", 404);
+    }
+
+    // check for user's cart
+    const cart = await prisma.cart.findUnique({
+        where: { userId },
+        include: { items: { include: { product: true } } },
+    });
+    if (!cart || cart.items.length === 0) {
+        throw new AppError("Your cart is empty", 400);
+    }
+
+    // building order
+    const order = await prisma.$transaction(async (tx) => {
+        let subtotal = 0;
+        const orderItemsData: Prisma.OrderItemCreateManyOrderInput[] = [];
+
+        for (const item of cart.items) {
+            if (!item.product.isActive) {
+                throw new AppError(
+                    `"${item.product.name}" is no longer available`,
+                    400,
+                );
+            }
+
+            // double check for product's stock, if stock is enough decrement the quantity
+            const result = await tx.product.updateMany({
+                where: { id: item.productId, stock: { gte: item.quantity } },
+                data: { stock: { decrement: item.quantity } },
+            });
+
+            // if stock is not enough, throw error
+            if (result.count === 0) {
+                throw new AppError(
+                    `Insufficient stock for "${item.product.name}"`,
+                    409,
+                );
+            }
+
+            // calculate subtotal and save items info
+            const lineSubtotal = Number(item.product.price) * item.quantity;
+            subtotal += lineSubtotal;
+
+            orderItemsData.push({
+                productId: item.productId,
+                productName: item.product.name,
+                productPrice: item.product.price,
+                quantity: item.quantity,
+                subtotal: lineSubtotal,
+            });
+        }
+
+		// calculate total && create order
+        const tax = Number((subtotal * env.TAX_RATE).toFixed(2));
+        const shippingCost = env.SHIPPING_COST;
+        const total = Number((subtotal + tax + shippingCost).toFixed(2));
+
+        const createdOrder = await tx.order.create({
+            data: {
+                userId,
+                addressId,
+                subtotal,
+                tax,
+                shippingCost,
+                total,
+                items: { createMany: { data: orderItemsData } },
+            },
+            include: { items: true, address: true },
+        });
+
+		// remove items from user's cart
+        await tx.cartItem.deleteMany({
+            where: { cartId: cart.id },
+        });
+
+        return createdOrder;
+    });
+
+    // setup stripe's paymentIntent
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: Math.round(Number(order.total) * 100),
+        currency: env.CURRENCY,
+        metadata: { orderId: order.id, userId },
+    });
+
+    // update order
+    const updatedOrder = await prisma.order.update({
+        where: { id: order.id },
+        data: { paymentIntentId: paymentIntent.id },
+        include: { items: true, address: true },
+    });
+
+    return { order: updatedOrder, clientSecret: paymentIntent.client_secret };
+};
