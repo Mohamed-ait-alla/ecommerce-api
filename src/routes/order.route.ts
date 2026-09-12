6dd7a4a5ee102391d72de5ca0efdbd0da14a5b96
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-09-12 15:30:51 -0400

    feat(routes/order): add a route for listing user orders action

--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { requireAuth } from "../middlewares/auth.middleware";
 import { validate } from "../middlewares/validate.middleware";
-import { checkoutSchema } from "../validators/order.validator";
+import {
+    checkoutSchema,
+    listOrdersQuerySchema,
+} from "../validators/order.validator";
 import * as orderController from "../controllers/order.controller";
 
 const router = Router();
@@ -9,5 +12,6 @@ const router = Router();
 router.use(requireAuth);
 
 router.post('/checkout', validate(checkoutSchema), orderController.checkout);
+router.get('/', validate(listOrdersQuerySchema), orderController.listMyOrders);
 
 export default router;
