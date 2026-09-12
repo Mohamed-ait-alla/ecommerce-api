2bf75d0ca2be39a9e836b8e6fc846c10d4cca4c6
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-09-12 15:29:52 -0400

    feat(controllers/order): implement list user orders action

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -1,5 +1,6 @@
 import type { Response } from "express";
 import type { AuthenticatedRequest } from "../types/auth.types";
+import type { ListOrdersQuery } from "../validators/order.validator";
 import { sendResponse } from "../utils/apiResponse";
 import * as orderService from "../services/order.service";
 
@@ -13,4 +14,12 @@ const checkout = async (req: AuthenticatedRequest, res: Response) => {
     );
 };
 
-export { checkout };
+const listMyOrders = async (req: AuthenticatedRequest, res: Response) => {
+    const orders = await orderService.listUserOrders(
+        req.user!.id,
+        req.query as unknown as ListOrdersQuery,
+    );
+    sendResponse(res, 200, orders);
+};
+
+export { checkout, listMyOrders };
