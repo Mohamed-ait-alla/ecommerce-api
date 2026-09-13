8cbe028e648de6f75151dad1d545fefbe98fec8e
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-09-13 05:13:33 -0400

    feat(routes/order): add a route for get a user's order by id action

--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -4,6 +4,7 @@ import { validate } from "../middlewares/validate.middleware";
 import {
     checkoutSchema,
     listOrdersQuerySchema,
+	orderIdParamSchema,
 } from "../validators/order.validator";
 import * as orderController from "../controllers/order.controller";
 
@@ -13,5 +14,6 @@ router.use(requireAuth);
 
 router.post('/checkout', validate(checkoutSchema), orderController.checkout);
 router.get('/', validate(listOrdersQuerySchema), orderController.listMyOrders);
+router.get('/:id', validate(orderIdParamSchema), orderController.getMyOrder);
 
 export default router;
