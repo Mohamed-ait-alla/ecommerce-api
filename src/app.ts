2317429b5ab8b29d97414c2889b3f87eb4609f09
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-09-14 13:31:15 -0400

    perf(server): use compression for performance purposes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
 import cors from "cors";
+import compression from "compression";
 import authRoutes from "./routes/auth.route";
 import productRoutes from "./routes/product.route";
 import categoryRoutes from "./routes/category.route";
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
 app.use(cors());
+app.use(compression());
 
 // routes
 app.use('/api/v1/auth', authRoutes);
