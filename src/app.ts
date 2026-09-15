016a921366b54eb83b04636feddf8ece3603c2b4
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-09-15 10:49:30 -0400

    feat(app): apply a general rate limiter to all routes

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ import adminRoutes from "./routes/admin.route";
 import { errorHandler } from "./middlewares/error.middleware";
 import { notFoundHandler } from "./middlewares/notFound.middleware";
 import { requestLogger } from "./middlewares/requestLogger.middleware";
+import { generalLimiter } from "./middlewares/rateLimiter.middleware";
 
 
 export const app = express();
@@ -26,6 +27,7 @@ app.use(helmet());
 app.use(cors());
 app.use(compression());
 app.use(requestLogger);
+app.use(generalLimiter);
 
 // routes
 app.use('/api/v1/auth', authRoutes);
